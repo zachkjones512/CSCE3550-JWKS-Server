f45fb1778b26f4c29c36527acce46bfa54949e08
Zachary Jones <zj35635@gmail.com>
2024-12-06 13:42:27 -0600

    Added aes encryption to keys to store in the database along with a user registration route

--- keygen.py
+++ keygen.py
@@ -6,13 +6,22 @@ Project 2: JWKS Server SQLite integration
 keygen.py: Generate keys to a SQLite database that are encoded for JWKS implementation
 """
 
-import base64
+from base64 import b64encode, b64decode
 import time
 import sqlite3
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes #used for decryption
+from cryptography.hazmat.backends import default_backend #used in cipher
+import os
 
 sqlDB = "totally_not_my_privateKeys.db"
+
+if 'NOT_MY_KEY' not in os.environ: #create temporary env variable
+    temp_key = os.urandom(16)
+    os.environ['NOT_MY_KEY'] = b64encode(temp_key).decode('utf-8')
+    print("--temp key set")
+
 
 class RSAKey:
     def __init__(self, key_id=None, expire_time=1):
@@ -23,29 +32,80 @@ class RSAKey:
         )
         self.expire = int(time.time()) + (expire_time * 3600) #set expire time
         self.save_to_db() 
-    
+
+    def grab_key(): #grabs key from environment variable
+        env_key = os.environ.get('NOT_MY_KEY')
+        if not env_key:
+            print("ERROR: no NOT_MY_KEY env variable")
+        return b64decode(env_key) #decodes key
+
+    #aes decryption and encryption functions for the private key
+    def encrypt_data(self, data):
+
+        aes_key = RSAKey.grab_key()
+        cipher = Cipher(algorithms.AES(aes_key), modes.ECB(), backend=default_backend()) #ecb because no iv
+        encryptor = cipher.encryptor()
+
+
+        #create padding to ensure its a multiple of 16 bytes
+        if len(data) % 16 != 0:
+            data = data + b'\0' * (16 - len(data) % 16)
+
+        ciphertext = encryptor.update(data) + encryptor.finalize()
+        return ciphertext
+
+    def decrypt_data(encrypted): #can decrypt the key from the aes format if necessary
+        aes_key = RSAKey.grab_key()
+        cipher = Cipher(algorithms.AES(aes_key), modes.ECB(), backend=default_backend())  # Use ECB mode
+        decryptor = cipher.decryptor()
+
+        decrypted_data = decryptor.update(encrypted) + decryptor.finalize()
+
+        #remove padding
+        decrypted_data = decrypted_data.rstrip(b'\0')
+        
+        return decrypted_data #returns decrypted aes key
+
+
     #serialize the private key in PEM for db storage, decodes later.
     def serialize_private_key(self):
         return self.priv_key.private_bytes(
             encoding=serialization.Encoding.PEM,
             format=serialization.PrivateFormat.TraditionalOpenSSL,
             encryption_algorithm=serialization.NoEncryption()
-        ) #decoding to utf8 ensures storage
+        )
     
-    #save the generated private key to the db
+    #serializes public key for DB storage, just like above
+    def serialize_public_key(self):
+        public_key = self.priv_key.public_key()
+        return public_key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo            
+        ).decode('utf-8')
+    
+    #save the generated private key and its public key to the db
     def save_to_db(self):
         with sqlite3.connect(sqlDB) as conn: #This is used to close the transaction block and ensure save
             cursor = conn.cursor()
             cursor.execute("""
                 CREATE TABLE IF NOT EXISTS keys(
                     kid INTEGER PRIMARY KEY AUTOINCREMENT,
-                    key BLOB NOT NULL,
+                    key BLOB NOT NULL,                                       
                     exp INTEGER NOT NULL
                 )    
-           """) 
-            cursor.execute("""
-                INSERT INTO keys (key, exp) VALUES (?, ?)""", (self.serialize_private_key(), int(self.expire)))
+           """)
             conn.commit()
+            
+        serialized_key = self.serialize_private_key() 
+        encrypted_key = self.encrypt_data(serialized_key)
+        if not encrypted_key:
+            print("error: encrypted private key is empty or non generated")
+
+        with sqlite3.connect(sqlDB) as key_conn:
+            key_cursor = key_conn.cursor()
+            key_cursor.execute("""
+                INSERT INTO keys (key, exp) VALUES (?, ?)""", (encrypted_key, int(self.expire)))
+            key_conn.commit()
 
 
     def expired(self):
@@ -53,7 +113,7 @@ class RSAKey:
         is_expired = time.time() > self.expire
         return is_expired
 
-
+    @staticmethod
     def serialize_jwks(kid, key, exp):
         #converts the key into the JWKS format
 
@@ -62,8 +122,8 @@ class RSAKey:
         pub_numbers = pub_key.public_numbers()
 
         #converts those numbers into the base64 format
-        n = base64.urlsafe_b64encode(pub_numbers.n.to_bytes(256, byteorder='big')).rstrip(b'=').decode('utf-8')
-        e = base64.urlsafe_b64encode(pub_numbers.e.to_bytes(3, byteorder='big')).rstrip(b'=').decode('utf-8')
+        n = b64encode(pub_numbers.n.to_bytes(256, byteorder='big')).rstrip(b'=').decode('utf-8')
+        e = b64encode(pub_numbers.e.to_bytes(3, byteorder='big')).rstrip(b'=').decode('utf-8')
 
         #generate json
         jwks = {
--- main.py
+++ main.py
@@ -8,12 +8,38 @@ main.py: Host web server using flask and authenticate JWTs
 
 from flask import Flask, jsonify, request #using flask for webserver
 from cryptography.hazmat.primitives import serialization
+from argon2 import PasswordHasher
 import time
 import jwt
 import sqlite3
+import uuid
 from keygen import RSAKey, sqlDB #imports RSAKey class from keygen.py
 
+
 app = Flask(__name__) #creates flask application
+
+#token bucket class for rate-limiting the auth route
+class token_bucket:
+    def __init__(self, rate: int, capacity: int):
+        self.rate = rate
+        self.capacity = capacity
+        self.tokens = capacity
+        self.last_refill = time.time()
+    
+    def refill(self):
+        #refill tokens after the time has passed
+        now = time.time()
+        elapsed = now - self.last_refill
+        self.tokens = min(self.capacity, self.tokens + elapsed * self.rate)
+        self.last_refill = now
+
+    def consume(self, count: int = 1) -> bool:
+        #returns true if tokens are avaialble and consumes token, count = tokens to consume
+        self.refill()
+        if self.tokens >= count:
+            self.tokens -= count
+            return True
+        return False
 
 #route to grab jwks
 @app.route('/.well-known/jwks.json', methods=['GET'])
@@ -26,8 +52,8 @@ def jwks(): #returns the JWKS with the public key
         cursor.execute("SELECT kid,key,exp FROM keys WHERE exp >= ?",  (int(time.time()),))
         rows = cursor.fetchall()
         for row in rows:
-            kid, key_en, exp = row #creates a tuple of each row with the kid, the key, and the exp
-            key = serialization.load_pem_private_key(key_en, password=None) #loads the private key
+            kid, public_key, exp = row #creates a tuple of each row with the kid, the key, and the exp
+            key = serialization.load_pem_public_key(public_key.encode('utf-8')) #loads the private key
             jwks_keys.append(RSAKey.serialize_jwks(str(kid), key, exp)) #use the serialize_jwks method to get the JWKS format
     
     jwks_output = {
@@ -36,45 +62,130 @@ def jwks(): #returns the JWKS with the public key
 
     return jsonify(jwks_output) #returns as a json
 
+#registration route
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+
+    username = data.get('username')
+    email = data.get('email')
+
+    if not username or not email:
+        return jsonify({"error: Username and email required"}), 400
+    
+    #generates password
+    password = str(uuid.uuid4())
+
+    #hashes password
+    hasher = PasswordHasher()
+    hashed = hasher.hash(password)
+
+    #store data and hashed password in users table
+    with sqlite3.connect(sqlDB) as conn:
+        cursor = conn.cursor()
+        cursor.execute("""
+            INSERT INTO users (username, password_hash, email)
+            VALUES (?, ?, ?)
+        """, (username, hashed, email))
+        conn.commit()
+        #return original to user
+        return jsonify({"password": password}), 201
+
 #authentication and JWT issue route
 @app.route('/auth', methods=['POST'])
 def auth():
-    use_expired = request.args.get('expired', default=False, type=bool) #checks if expired flag is set
-
-    with sqlite3.connect(sqlDB) as conn: #Read all the entries in the database
-        cursor = conn.cursor()
-
-    #if expired flag is set select expired key, otherwise select non-expired keys
-    if use_expired:
-        cursor.execute("SELECT kid,key,exp FROM keys WHERE exp < ?",  (int(time.time()),))
-        row = cursor.fetchone() #grabs first matching row
-        if row is None:
-            print("Expired key not in database! ERROR!")
-        else:
-            print("Expired key added to database")
-    else: 
-        cursor.execute("SELECT kid,key,exp FROM keys WHERE exp >= ?",  (int(time.time()),))
-        row = cursor.fetchone() #grabs first matching row
-        if row is None: #if no row found
-            return jsonify({"error": "No valid keys available"}), 400 
     
-    kid, key_en, exp = row #creates a tuple of the row with the kid, the key, and the exp
-    key = serialization.load_pem_private_key(key_en, password=None) #loads the private key  
+    #initialize token bucket to rate limit to 10 requests per second
+    rate_limiter = token_bucket(rate=10, capacity=10)
 
+    if not rate_limiter.consume():
+        return jsonify({"error": "Rate limit exceeded, try again later."}), 429
+
+    data = request.get_json()
+
+    username = data.get('username')
+    password = data.get('password')
+
+    if not username or not password:
+        return jsonify({"error: Username and password required"}), 400
+    
+    with sqlite3.connect(sqlDB) as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT id, password_hash FROM users WHERE username = ?", (username,))
+        user = cursor.fetchone()
+        conn.commit()
+        if not user:
+            return jsonify({"error": "No matching user or password"}), 401
+        
+        user_id, hash = user
+
+        #verify password
+        hasher = PasswordHasher()
+        try:
+            hasher.verify(hash, password)
+        except:
+                return jsonify("error: no matching user or password")
+
+        user_ip = request.remote_addr
+        
+        with sqlite3.connect(sqlDB) as log_conn:
+            log_cursor = log_conn.cursor()
+            log_cursor.execute("""
+                INSERT INTO auth_logs (request_ip, user_id)
+                VALUES (?, ?)
+            """, (user_ip, user_id))
+            log_conn.commit()
+
+        with sqlite3.connect(sqlDB) as key_conn:
+            key_cursor = key_conn.cursor()
+            key_cursor.execute("SELECT kid, key, exp FROM keys WHERE exp >= ?", (int(time.time()),))
+            row = key_cursor.fetchone()
+            key_conn.commit()
+            if row is None:
+                return jsonify({"error": "No valid keys available"}), 400
+            
+    kid, key_en, exp = row  #creates a tuple of the row with the kid, the encrypted key, and the exp
+    #decrypt private key to sign
+    private_key_pem = RSAKey.decrypt_data(key_en) 
+    private_key_obj = serialization.load_pem_private_key(private_key_pem, password=None)
+
+    #SEND PRIV KEY WITH JWT
     payload = {
         "sub": "12345678910",
         "name": "Test Name",
         "iat": int(time.time()),
-        "exp": int(exp)  #set to a valid expire time
+        "exp": int(exp),  #set to a valid expire time
     }
 
     #encode jwt with private key
-    token = jwt.encode(payload, key, algorithm='RS256', headers={"kid": str(kid)})
-    return jsonify({"token": token})
+    token = jwt.encode(payload, private_key_obj, algorithm='RS256', headers={"kid": str(kid)})
+    return jsonify({"token": token}, 200)
 
 if __name__ == '__main__':
     for i in range(5): #initializes a list of 5 RSA key pairs and saves them in DB
-        expire_time = -1 if i==4 else 1 #makes the last one expired
+        expire_time = 1
         key = RSAKey(expire_time=expire_time) 
 
+    with sqlite3.connect(sqlDB) as conn: #create users and auth_logs table
+        cursor = conn.cursor()
+        cursor.execute("""
+                        CREATE TABLE IF NOT EXISTS users(
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        username TEXT NOT NULL UNIQUE,
+                        password_hash TEXT NOT NULL,
+                        email TEXT UNIQUE,
+                        date_registered TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                        last_login TIMESTAMP      
+            )   
+        """)
+        cursor.execute("""
+                       
+                        CREATE TABLE IF NOT EXISTS auth_logs(
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        request_ip TEXT NOT NULL,
+                        request_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                        user_id INTEGER,  
+                        FOREIGN KEY(user_id) REFERENCES users(id)
+            );
+        """) 
     app.run(port=8080) #listens on port 8080
